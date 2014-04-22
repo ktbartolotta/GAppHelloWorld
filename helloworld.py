7f271460a50ce4ae302d6594a2447b1781d9f8f5
Keif <mashidin@hotmail.com>
2014-04-22 18:07:03 -0400

    Initial commit.

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+"""A simple webapp2 server."""
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.write('Goodbye, cruel World...')
+
+application = webapp2.WSGIApplication([
+  ('/', MainPage)
+], debug=True)
