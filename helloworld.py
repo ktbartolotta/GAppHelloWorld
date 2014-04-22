28521b3787196bd7b3a5a9f80caf5682ec174e02
Keif <mashidin@hotmail.com>
2014-04-22 18:21:49 -0400

    Added google user api functionality.

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,18 @@
 """A simple webapp2 server."""
+from google.appengine.api import users
 
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Goodbye, cruel World...')
-
+	users = users.get_current_user()
+	
+	if user:
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Hello, ' + user.nickname())
+	else:
+		self.redirect(users.create_login_url(self.request.uri))
+		
 application = webapp2.WSGIApplication([
   ('/', MainPage)
 ], debug=True)
